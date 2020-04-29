d61ddc10cf107d10906f5a03e8f989392667d6ae
Hyojin Yoo <yoohyojin@gmail.com>
2020-04-29 18:55:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,10 +34,13 @@ def monopoly_with_third_tier
         one_piece_owned: 25,
         two_pieces_owned: 50,
         three_piece_owned: 100,
-        four_piece_owned: 200,
+        four_piece_owned: 200
       },
       :names => {
-        
+        :reading_railroad => {},
+        :reading_railroad => {},
+        :reading_railroad => {},
+        :reading_railroad => {}
       }
     }
   }
